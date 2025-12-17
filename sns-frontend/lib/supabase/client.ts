062fbbdef649aa877bad50663f6495441d1ecb36
hassudao <141802250+hassudao@users.noreply.github.com>
2025-12-18 03:09:22 +0900

    Delete sns-frontend directory

--- sns-frontend/lib/supabase/client.ts
+++
@@ -1,8 +0,0 @@
-// lib/supabase/client.ts
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-
-// ブラウザ（クライアントコンポーネント）から利用するためのクライアントを作成
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
