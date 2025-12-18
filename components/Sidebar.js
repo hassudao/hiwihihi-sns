7d14aa6e412931a1b96dad3cd7c7f49263e1fc45
hassudao <141802250+hassudao@users.noreply.github.com>
2025-12-18 16:02:06 +0000

    update project

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -39,7 +39,7 @@ export default function Sidebar() {
             <img 
               src="/logo.png" 
               alt="Logo" 
-              className="w-10 h-10 object-contain" 
+              className="w-16 h-10 object-contain" 
             />
           </Link>
         </div>
